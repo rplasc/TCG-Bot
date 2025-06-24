64e6d88a82165b87b349eb8915a6b8690d2c86a1
Raul <148407066+rplasc@users.noreply.github.com>
2025-06-23 22:06:28 -0700

    Fixed missing import error in Shop View

--- src/shop/views.py
+++ src/shop/views.py
@@ -1,6 +1,7 @@
 from discord import ui, SelectOption, Interaction, ButtonStyle, Embed, Color
 from .logic import SHOP_PACKS, handle_shop_purchase, handle_card_purchase, CARD_SHOP_PRICES, RARITY_EMOJIS
-from src.db.db import get_daily_shop_cards, format_duration, get_seconds_until_next_rotation
+from src.db.db import get_daily_shop_cards
+from src.utils.time import format_duration, get_seconds_until_next_rotation
 
 class ShopView(ui.View):
     def __init__(self, user_id: int):
